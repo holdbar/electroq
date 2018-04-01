90aada4d710d0b16f5156ba15dd0f0cbcac1ce2a
Holdbar <holdbarhet@gmail.com>
2018-04-01 12:48:21 +0300

    changed project structure

---
+++ temp/core/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from core import views as core_views
+
+
+from rest_framework.authtoken import views
+
+
+urlpatterns = [
+    url(r'^api-token-auth/', views.obtain_auth_token),
+    url(r'^', core_views.index),
+]
+
