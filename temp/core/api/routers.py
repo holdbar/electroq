42a7e3565966b3315ab9814a171e549c3340a23b
Holdbar <holdbarhet@gmail.com>
2018-04-01 12:48:59 +0300

    removed temp files

--- temp/core/api/routers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from core.api.viewsets import UserViewSet
-
-router = DefaultRouter()
-
-# registering viewsets
-router.register(r"user", UserViewSet)
